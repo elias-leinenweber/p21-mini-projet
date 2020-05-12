f7e4bee4b605ccc4f954f5b41f94af3cd7bff0e4
PepeteLaMouette <lilianbouvier.music@gmail.com>
2020-05-12 17:40:27 +0200

    ajouté DessinTest1.java

---
+++ src/tests/java/dessinvectoriel/DessinTest1.java
@@ -0,0 +1,91 @@
+package dessinvectoriel;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class DessinTest1 {
+
+    @BeforeEach
+    void setUp()
+    {
+        Dessin d = new Dessin("La Joconde","Leonard de Vinci",200,200);
+    }
+
+    @Test
+    void testListerFigures(){
+        
+    }
+
+    @Test
+    void testGetAuteur(){
+        assertEquals("Leonard de Vinci",d.getAuteur());
+    }
+
+    @Test
+    void testGetTitre(){
+        assertEquals("La Joconde",d.getTitre());
+    }
+
+    @Test
+    void testGetDateCreation(){
+        assertEquals(LocalDate.now(),d.getDateCreation());
+    }
+
+    @Test
+    void testGetLargeur(){
+        assertEquals(200,d.getLargeur());
+    }
+
+    @Test
+    void testGetHauteur(){
+        assertEquals(200,d.getAuteur());
+    }
+
+    @Test
+    void testToString(){
+        assertEquals("Auteur : " + d.getAuteur() + "\n" +
+                    "Titre : " + d.getTitre() + "\n" +
+                    "Date de création : " + d.getDateCreation().toString() + "\n" +
+                    "Dimensions : " + d.getLargeur() + "x" + d.getHauteur()
+                    "Figures : "+d.listerFigures().toString(),d.toString());
+    }
+
+    @Test
+    void testSetAuteur(){
+        d.setAuteur("El Chapo");
+        assertEquals("El Chapo",d.getAuteur());
+    }
+
+    @Test
+    void testSetTitre(){
+        d.setTitre("ednocoJ aL");
+        assertEquals(d.getTitre());
+    }
+
+    @Test
+    void testSetDateCreation(){
+        d.setDateCreation(LocalDate.of(2000,1,1));
+        assertEquals(LocalDate.of(2000,1,1),d.getDateCreation());
+    }
+
+    @Test
+    void testSetLargeur(){
+        d.setLargeur(300);
+        assertEquals(300,d.getLargeur());
+    }
+
+    @Test
+    void testSetHauteur(){
+        d.setHauteur(300);
+        assertEquals(300,d.getHauteur());
+    }
+
+    @Test
+    void testAjouterFigure(){
+
+    }
+
+}
