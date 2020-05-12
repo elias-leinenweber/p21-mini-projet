3020f5bbf4cc05ebad29a9d871f79e2778fb197c
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-05-12 21:10:21 +0200

    fini squelette

--- src/main/java/dessinvectoriel/Rectangle.java
+++ src/main/java/dessinvectoriel/Rectangle.java
@@ -1,18 +1,66 @@
 package dessinvectoriel;
 
+import java.awt.*;
+
 public class Rectangle extends Surface {
-	private double longueur;
-	private double largeur;
+	private final double longueur;
+	private final double largeur;
+
 
 	public Rectangle(Vecteur position, double longueur, double largeur)
 	{
-		// TODO implement
+		super(position);
+		this.longueur = longueur;
+		this.largeur = largeur;
 	}
 
+	public Rectangle(Vecteur position, Angle orientation, double longueur, double largeur)
+	{
+		super(position, orientation);
+		this.longueur = longueur;
+		this.largeur = largeur;
+	}
+
+	public Rectangle(Vecteur position, Angle orientation, double longueur, double largeur, Color couleurTrait, Integer epaisseurTrait, Color couleurRemplissage)
+	{
+		super(position, orientation, couleurTrait, epaisseurTrait, couleurRemplissage);
+		this.longueur = longueur;
+		this.largeur = largeur;
+	}
+
+
+	@Override
+	public double surface()
+	{
+		return longueur * largeur;
+	}
+
+	@Override
+	public double perimetre()
+	{
+		return 2 * (longueur + largeur);
+	}
+
+	public double getLongueur()
+	{
+		return longueur;
+	}
 
 	public double getLargeur()
 	{
 		return largeur;
+	}
+
+	public Vecteur[] getSommets()
+	{
+		// TODO implement
+		return null;
+	}
+
+	@Override
+	public void dessiner(Graphics2D g)
+	{
+		// TODO implement
 	}
 
 	public Rectangle copier()
@@ -25,5 +73,12 @@ public class Rectangle extends Surface {
 	public void redimensionner(double facteur)
 	{
 		// TODO implement
+	}
+
+	@Override
+	public String toString()
+	{
+		// TODO implement
+		return null;
 	}
 }
