8de1d1989effacc76a0746d6a54c06a84aa59a9c
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-05-12 20:30:14 +0200

    fini squelette Vecteur

--- src/main/java/dessinvectoriel/Vecteur.java
+++ src/main/java/dessinvectoriel/Vecteur.java
@@ -4,9 +4,67 @@ public class Vecteur {
 	private double x;
 	private double y;
 
+
 	public Vecteur(double x, double y)
 	{
 		this.x = x;
 		this.y = y;
+	}
+
+	public Vecteur(double rayon, Angle orientation)
+	{
+		// TODO implement
+	}
+
+
+	public double getX()
+	{
+		return x;
+	}
+
+	public double getY()
+	{
+		return y;
+	}
+
+	public Vecteur ajouter(Vecteur autre)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public Vecteur oppose()
+	{
+		// TODO implement
+		return null;
+	}
+
+	public Vecteur soustraire(Vecteur autre)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public Vecteur multiplier(double facteur)
+	{
+		return new Vecteur(facteur * x, facteur * y);
+	}
+
+	public double longueur()
+	{
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public Angle orientation()
+	{
+		// TODO implement
+		return null;
+	}
+
+	@Override
+	public String toString()
+	{
+		// TODO implement
+		return null;
 	}
 }
