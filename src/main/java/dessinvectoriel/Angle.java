6263be5180de5e6363ad893a7acd5f7f7b957e83
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-05-12 20:23:30 +0200

    fini squelette Angle

--- src/main/java/dessinvectoriel/Angle.java
+++ src/main/java/dessinvectoriel/Angle.java
@@ -1,9 +1,83 @@
 package dessinvectoriel;
 
 public class Angle {
-    private double mesure;
+	public static final Angle NUL = Angle.degres(0);
+	public static final Angle DROIT = Angle.degres(90);
+	public static final Angle PLAT = Angle.degres(180);
 
-    private Angle(double rad){
-        // TODO implement
-    }
+	private double mesure;
+
+
+	public Angle(double rad)
+	{
+		// TODO implement
+	}
+
+
+	public static Angle degres(double deg)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public static Angle radians(double rad)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public double getDegres()
+	{
+		// TODO implement
+		return 0.0;
+	}
+
+	public double getRadians()
+	{
+		// TODO implement
+		return 0.0;
+	}
+
+	public Angle ajouterDegres(double deg)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public Angle ajouterRadians(double rad)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public Angle ajouter(Angle theta)
+	{
+		// TODO implement
+		return null;
+	}
+
+	public double sin()
+	{
+		// TODO implement
+		return 0.0;
+	}
+
+	public double cos()
+	{
+		// TODO implement
+		return 0.0;
+	}
+
+	public static Angle atan(double dX, double dY)
+	{
+		// TODO implement
+		return null;
+	}
+
+	@Override
+	public String toString()
+	{
+		// TODO implement
+		return null;
+	}
 }
