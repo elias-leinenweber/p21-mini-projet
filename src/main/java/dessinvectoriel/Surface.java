4da9422c81bd5d1bbc539e3babbd3ba7e119505d
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-05-12 21:02:11 +0200

    fini squelette

--- src/main/java/dessinvectoriel/Surface.java
+++ src/main/java/dessinvectoriel/Surface.java
@@ -6,8 +6,51 @@ public abstract class Surface extends Figure {
 	private static Color couleurRemplissageParDefaut = null;
 	private Color couleurRemplissage;
 
+
+	public Surface(Vecteur position)
+	{
+		super(position);
+	}
+
+	public Surface(Vecteur position, Angle orientation)
+	{
+		super(position, orientation);
+	}
+
+	public Surface(Vecteur position, Angle orientation, Color couleurTrait, Integer epaisseurTrait, Color couleurRemplissage)
+	{
+		super(position, orientation, couleurTrait, epaisseurTrait);
+		this.couleurRemplissage = couleurRemplissage;
+	}
+
+
 	public void setCouleurRemplissage(Color couleurRemplissage)
 	{
+		this.couleurRemplissage = couleurRemplissage;
+	}
+
+	public Color getCouleurRemplissage()
+	{
+		return couleurRemplissage;
+	}
+
+	public abstract double surface();
+
+	public abstract double perimetre();
+
+	protected boolean initRemplissage(Graphics2D g)
+	{
 		// TODO implement
+		return false;
+	}
+
+	public static Color getCouleurRemplissageParDefaut()
+	{
+		return couleurRemplissageParDefaut;
+	}
+
+	public static void setCouleurRemplissageParDefaut(Color couleurRemplissageParDefaut)
+	{
+		Surface.couleurRemplissageParDefaut = couleurRemplissageParDefaut;
 	}
 }
